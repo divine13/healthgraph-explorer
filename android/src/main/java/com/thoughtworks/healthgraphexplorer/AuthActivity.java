1df01ff6152d2f8f58b720b1415cefd805642920
Serge Gebhardt <serge.gebhardt@gmail.com>
2013-05-28 18:50:15 -0300

    Add Auth Activity and register URI protocol.

---
+++ android/src/main/java/com/thoughtworks/healthgraphexplorer/AuthActivity.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.healthgraphexplorer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AuthActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
--- android/src/main/java/com/thoughtworks/healthgraphexplorer/HelloAndroidActivity.java
+++ android/src/main/java/com/thoughtworks/healthgraphexplorer/HelloAndroidActivity.java
@@ -1,30 +1,49 @@
 package com.thoughtworks.healthgraphexplorer;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class HelloAndroidActivity extends Activity {
 
     /**
      * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button callUrlButton = (Button) findViewById(R.id.callUrlButton);
+        callUrlButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "healthex://auth.com/blah";
+                Log.i("xxx", "url: " + url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                Log.i("xxx", "will start intent");
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(com.thoughtworks.healthgraphexplorer.R.menu.main, menu);
-	return true;
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(com.thoughtworks.healthgraphexplorer.R.menu.main, menu);
+        return true;
     }
+
 
 }
 
