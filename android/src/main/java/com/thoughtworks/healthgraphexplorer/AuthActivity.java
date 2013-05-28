df13ea131ab70292a7a315113ed14e7fd5d387ae
Serge Gebhardt <serge.gebhardt@gmail.com>
2013-05-28 20:16:23 -0300

    Organize imports

--- android/src/main/java/com/thoughtworks/healthgraphexplorer/AuthActivity.java
+++ android/src/main/java/com/thoughtworks/healthgraphexplorer/AuthActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class AuthActivity extends Activity {
