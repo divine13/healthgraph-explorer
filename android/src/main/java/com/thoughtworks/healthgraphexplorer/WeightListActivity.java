8fe21af5dd5974a9f73983195eec664c4df7d7ad
Serge Gebhardt <serge.gebhardt@gmail.com>
2013-08-11 23:37:49 -0300

    Add getWeightSetFeed to API

--- android/src/main/java/com/thoughtworks/healthgraphexplorer/WeightListActivity.java
+++ android/src/main/java/com/thoughtworks/healthgraphexplorer/WeightListActivity.java
@@ -10,8 +10,13 @@ import com.octo.android.robospice.persistence.exception.SpiceException;
 import com.octo.android.robospice.request.listener.RequestListener;
 import com.octo.android.robospice.request.retrofit.RetrofitSpiceRequest;
 import com.thoughtworks.healthgraphexplorer.service.HealthGraphApi;
-import com.thoughtworks.healthgraphexplorer.service.model.User;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import retrofit.client.Response;
 import roboguice.inject.InjectView;
 
 public class WeightListActivity extends BaseActivity {
@@ -36,24 +41,45 @@ public class WeightListActivity extends BaseActivity {
     }
 
     public void fetchList(View view) {
-        RetrofitSpiceRequest<User, HealthGraphApi> request =
-                new RetrofitSpiceRequest<User, HealthGraphApi>(User.class, HealthGraphApi.class) {
+        RetrofitSpiceRequest<Response, HealthGraphApi> request =
+                new RetrofitSpiceRequest<Response, HealthGraphApi>(Response.class, HealthGraphApi.class) {
                     @Override
-                    public User loadDataFromNetwork() throws Exception {
-                        return getService().user();
+                    public Response loadDataFromNetwork() throws Exception {
+                        return getService().getWeightSetFeed();
                     }
                 };
 
-        RequestListener<User> requestListener = new RequestListener<User>() {
+        RequestListener<Response> requestListener = new RequestListener<Response>() {
             @Override
             public void onRequestFailure(SpiceException spiceException) {
                 Log.d("xxx", "request failed: " + spiceException);
             }
 
             @Override
-            public void onRequestSuccess(User user) {
-                Log.d("xxx", "request successful: " + user);
-                textView.setText(user.toString());
+            public void onRequestSuccess(Response response) {
+                Log.d("xxx", "request successful: " + response.toString());
+
+                InputStream inputStream = null;
+                try {
+                    inputStream = response.getBody().in();
+                } catch (IOException e) {
+                } finally {
+                    if (inputStream != null) {
+                        try {
+                            inputStream.close();
+                        } catch (IOException e) {
+                        }
+                    }
+                }
+
+                String body = null;
+                try {
+                    body = IOUtils.toString(inputStream, "UTF-8");
+                } catch (IOException e) {
+                }
+
+                textView.setText("Header: " + response.getHeaders().toString()
+                        + "\n\nBody: " + body);
             }
         };
 
--- android/src/main/java/com/thoughtworks/healthgraphexplorer/service/HealthGraphApi.java
+++ android/src/main/java/com/thoughtworks/healthgraphexplorer/service/HealthGraphApi.java
@@ -2,10 +2,21 @@ package com.thoughtworks.healthgraphexplorer.service;
 
 import com.thoughtworks.healthgraphexplorer.service.model.User;
 
+import retrofit.client.Response;
 import retrofit.http.GET;
-import retrofit.http.POST;
+import retrofit.http.Headers;
+
+import static com.thoughtworks.healthgraphexplorer.service.HealthGraphService.HealthGraphDynamicPath;
 
 public interface HealthGraphApi {
+    String ACCEPT = "Accept: ";
+
     @GET("/user")
-    User user();
+    @Headers(ACCEPT + "application/vnd.com.runkeeper.User+json")
+    User getUser();
+
+    @GET("/weight")
+    @HealthGraphDynamicPath
+    @Headers(ACCEPT + "application/vnd.com.runkeeper.WeightSetFeed+json")
+    Response getWeightSetFeed();
 }
