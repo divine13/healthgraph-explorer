5a690f16921f68173f9d14b143d58a8a5c9b3ae5
Serge Gebhardt <serge.gebhardt@gmail.com>
2013-08-12 00:46:42 -0300

    Store accessToken to shared Prefs

--- android/src/main/java/com/thoughtworks/healthgraphexplorer/MainActivity.java
+++ android/src/main/java/com/thoughtworks/healthgraphexplorer/MainActivity.java
@@ -3,16 +3,55 @@ package com.thoughtworks.healthgraphexplorer;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 
 import com.thoughtworks.healthgraphexplorer.service.HealthGraphAuthManager;
 
+import org.apache.commons.lang3.StringUtils;
+
+import roboguice.inject.InjectView;
+
 public class MainActivity extends BaseActivity {
+    private static final String SHARED_PREFS_HEALTH_GRAPH_AUTH = "HealthGraphAuth";
+    private static final String SHARED_PREFS_KEY_ACCESS_TOKEN = "AccessToken";
+
+    @InjectView(R.id.authButton)
+    Button authButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String accessTokenAuthManager = HealthGraphAuthManager.getInstance().getAccessToken();
+        String accessTokenSharedPrefs = getSharedPreferences(SHARED_PREFS_HEALTH_GRAPH_AUTH,
+                MODE_PRIVATE).getString(SHARED_PREFS_KEY_ACCESS_TOKEN, null);
+
+        authButton.setVisibility(View.VISIBLE);
+
+        if (accessTokenAuthManager != null || accessTokenSharedPrefs != null) {
+            authButton.setVisibility(View.GONE);
+
+            if (accessTokenAuthManager == null) {
+                // set from shared prefs to auth manager
+                HealthGraphAuthManager.getInstance().setAccessToken(accessTokenSharedPrefs);
+            } else if (accessTokenSharedPrefs == null
+                    || !StringUtils.equals(accessTokenAuthManager, accessTokenSharedPrefs)) {
+                // set from auth manager to shared prefs
+                getSharedPreferences(SHARED_PREFS_HEALTH_GRAPH_AUTH, MODE_PRIVATE).edit()
+                        .putString(SHARED_PREFS_KEY_ACCESS_TOKEN, accessTokenAuthManager).apply();
+            }
+        }
+
+        Log.d("xxx", "AccessToken: " + HealthGraphAuthManager.getInstance().getAccessToken());
     }
 
     @Override
--- android/src/main/java/com/thoughtworks/healthgraphexplorer/service/HealthGraphAuthManager.java
+++ android/src/main/java/com/thoughtworks/healthgraphexplorer/service/HealthGraphAuthManager.java
@@ -103,4 +103,11 @@ public class HealthGraphAuthManager {
         return Strings.notEmpty(authCode);
     }
 
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
 }
